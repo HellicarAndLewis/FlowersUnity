fe4a96cbd23f0d76708aa4c0d93aaa02acfeceb1
Chris Mullany <me@chrismullany.com>
2016-07-22 15:35:48 +0100

    Basic lighting controller

---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LightPreset
+{
+	public Color colour;
+	public Vector3 eulerAngle;
+	//void LightPreset(Color colour, Vector3 eulerAngle)
+}
+
+public class LightController : MonoBehaviour
+{
+	// --------------------------------------------------------------------------------------------------------
+	//
+	public Light sunLight;
+	public LightPreset[] presets = new LightPreset[4];
+
+	private LightPreset current;
+	private LightPreset previous;
+	private LightPreset target;
+
+	// --------------------------------------------------------------------------------------------------------
+	//
+	void Start()
+	{
+		if(!sunLight)
+			sunLight = FindObjectOfType<Light>();
+
+		// Dawn
+		if(presets[0] == null)
+			presets[0] = new LightPreset(){colour= Color.cyan, eulerAngle = new Vector3(27, 80, 0)};
+		// Daylight
+		if(presets[1] == null)
+			presets[1] = new LightPreset(){colour= Color.yellow, eulerAngle= new Vector3(27, 80, 0)};
+		// Dusk
+		if(presets[2] == null)
+			presets[2] = new LightPreset(){colour= Color.red, eulerAngle= new Vector3(27, 80, 0)};
+
+		current = presets[0];
+		Preset(ShowMode.Daytime);
+	}
+
+	// --------------------------------------------------------------------------------------------------------
+	//
+	void Update()
+	{
+		if(Input.GetKeyDown("1")) Preset(ShowMode.Dawn);
+		if(Input.GetKeyDown("2")) Preset(ShowMode.Daytime);
+		if(Input.GetKeyDown("3")) Preset(ShowMode.Dusk);
+			
+		if(sunLight) {
+			sunLight.color = current.colour;
+			var rotation = sunLight.transform.localRotation;
+			rotation.eulerAngles = current.eulerAngle;
+			sunLight.transform.localRotation = rotation;
+		}
+	}
+
+	// --------------------------------------------------------------------------------------------------------
+	//
+	public void Preset(ShowMode mode, float duration = 0)
+	{
+		var index = (int)mode;
+		previous = current;
+		target = presets[index];
+		current = presets[index];
+	}
+}
---
+++ Assets/Scripts/ShowController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum ShowMode
+{
+	Dawn=0, Daytime, Dusk, Night
+}
+
+public class ShowController : MonoBehaviour
+{
+
+	void Start()
+	{
+	
+	}
+
+	void Update()
+	{
+	
+	}
+}
