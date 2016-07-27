a0526ac58b2526086556333d1f83c7f4d38e9dc9
Bentleyj <jgbentley10@gmail.com>
2016-07-27 15:04:48 +0100

    Added MultiScreen Test Scene with a gui on the first screen but not the output screen

--- Assets/Scripts/boxController.cs
+++ Assets/Scripts/boxController.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine.Events;
 using System.Collections;
 
 public class boxController : MonoBehaviour {
 
     public onsetDetector onset;
-    public GameObject box;
+    public GameObject rock;
     public float onsetConfidence;
+    [Range(0, 5.0f)]
+    public float speed;
 
 	// Use this for initialization
 	void Start () {
@@ -17,8 +20,20 @@ public class boxController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        box.transform.localScale = new Vector3(1.0f, onset.onsetTotal,1.0f);
-        onsetConfidence = onset.onsetTotal;
+        //box.transform.localScale = new Vector3(1.0f, onset.onsetTotal,1.0f);
+        //onsetConfidence = onset.onsetTotal;
 
+        float x = rock.transform.eulerAngles.x;
+        float y = rock.transform.eulerAngles.y;
+        float z = rock.transform.eulerAngles.z;
+
+        y += speed;
+
+        rock.transform.eulerAngles = new Vector3(x, y, z);
+    }
+
+    public void onSliderChanged(float _speed)
+    {
+        speed = _speed * 5.0f;
     }
 }
