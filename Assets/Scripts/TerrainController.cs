1cf6fb06578fde28e78f89bddaf120e53f216c0a
Chris Mullany <me@chrismullany.com>
2016-07-27 12:34:46 +0100

    Boosted terrain flower size

--- Assets/Scripts/TerrainController.cs
+++ Assets/Scripts/TerrainController.cs
@@ -125,7 +125,7 @@ public class TerrainController : MonoBehaviour
         {
             var particle = particles[i];
             particle.enabled = (i < numParticlesDesired) ? 1 : 0;
-            particle.size = Random.Range(6, 6);
+            particle.size = 15;// Random.Range(6, 6);
             if (i < baseVertices.Length)
                 particle.position = transform.position + baseVertices[i] + (baseNormals[i] * Random.Range(2, 6)) + new Vector3(Random.Range(0,2),Random.Range(0, 2),0);
             else
