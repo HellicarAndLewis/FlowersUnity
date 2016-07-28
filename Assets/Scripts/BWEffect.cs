b35f4322a8a20f93756a8d6da2ec9ce3ea730d08
Bentleyj <jgbentley10@gmail.com>
2016-07-28 18:44:52 +0100

    Added test Shader scenes with a few effects!

---
+++ Assets/Scripts/BWEffect.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class BWEffect : MonoBehaviour {
+
+    public float intensity;
+    private Material material;
+	// Use this for initialization
+	void Awake () {
+        material = new Material(Shader.Find("Hidden/BWDiffuse"));
+	}
+	
+	// Update is called once per frame
+	void OnRenderImage (RenderTexture source, RenderTexture destination) {
+        if (intensity == 0) {
+            Graphics.Blit(source, destination);
+            return;
+        }
+
+        material.SetFloat("_bwBlend", intensity);
+        Graphics.Blit(source, destination, material);
+	}
+}
