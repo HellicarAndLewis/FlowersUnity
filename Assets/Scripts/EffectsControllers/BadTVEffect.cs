7681bc51cf1d237f1a4f2960caa1d3c23ab49a75
Bentleyj <jgbentley10@gmail.com>
2016-08-03 12:57:39 +0100

    Updated Toggle Controller and Added callbacks for 2D effects so we can control them with the GUI

--- Assets/Scripts/EffectsControllers/BadTVEffect.cs
+++ Assets/Scripts/EffectsControllers/BadTVEffect.cs
@@ -33,4 +33,39 @@ public class BadTVEffect : MonoBehaviour
 
         Graphics.Blit(source, destination, material);
     }
+
+    public void OnAmount(float _val)
+    {
+        float m = 1.0f;
+        float b = 0.0f;
+        amount = m * _val + b;
+    }
+
+    public void OnDistortion(float _val)
+    {
+        float m = 10.0f;
+        float b = 0.0f;
+        distortion = m * _val + b;
+    }
+
+    public void OnDistortion2(float _val)
+    {
+        float m = 1.0f;
+        float b = 0.0f;
+        distortion2 = m * _val + b;
+    }
+
+    public void OnSpeed(float _val)
+    {
+        float m = 1.0f;
+        float b = 0.0f;
+        speed = m * _val + b;
+    }
+
+    public void OnRollSpeed(float _val)
+    {
+        float m = 20.0f;
+        float b = 0.0f;
+        amount = m * _val + b;
+    }
 }
